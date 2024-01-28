4a725e5a00b0af1eaaaadc180a276d6425c323b8
4Nameless4 <623507166@qq.com>
2024-01-28 15:01:34 +0800

    markdown article todo

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -1,5 +1,39 @@
 import style from "./page.module.css";
+import Markdown from "react-markdown";
+import { Prism } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-export default function ArticlePage(props: { params: { id: string } }) {
-  return <div>ArticlePage {props?.params?.id ?? null}</div>;
+export default async function ArticlePage(props: { params: { id: string } }) {
+  const articleID = props.params.id;
+  const markdown = `Here is some JavaScript code:
+
+  \`\`\`html
+  console.log('It 111!')
+  \`\`\`
+  `;
+
+  return (
+    <div className="article-page-root">
+      <h3>title</h3>
+      <section>
+        <Markdown
+          components={{
+            code(props) {
+              const { children, className } = props;
+              const match = /language-(\w+)/.exec(className || "");
+              return match ? (
+                <Prism language={match[1]} style={dark}>
+                  {String(children).replace(/\n$/, "")}
+                </Prism>
+              ) : (
+                <code className={className}>{children}</code>
+              );
+            },
+          }}
+        >
+          {markdown}
+        </Markdown>
+      </section>
+    </div>
+  );
 }
--- app/article/edit/page.tsx
+++
@@ -1,5 +0,0 @@
-import style from "./page.module.css";
-
-export default function ArticleEditPage() {
-  return <section>ArticleEditPage</section>;
-}
