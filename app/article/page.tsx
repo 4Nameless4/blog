8139f125b911b45aa03467dda4da81a00a2712dd
4Nameless4 <623507166@qq.com>
2024-02-23 19:16:51 +0800

    change article overview page style

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,11 +1,12 @@
-// "use client";
+"use client";
 import Button from "antd/es/button";
 import style from "./page.module.css";
 import Link from "next/link";
 import { getArticleList } from "@/common/api";
-import { t_article_view } from "@/common/types";
-import { formatDate } from "@/common/utils";
+import { t_article_view, t_token_user } from "@/common/types";
+import { formatDate, getUser } from "@/common/utils";
 import { PlusOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
 
 function renderActicleList(list: t_article_view[] | null | false | undefined) {
   if (list) {
@@ -32,8 +33,31 @@ function renderActicleList(list: t_article_view[] | null | false | undefined) {
   return null;
 }
 
-export default async function ArticleOverviewPage() {
-  const list = await getArticleList();
+function renderCreateBtn() {
+  return (
+    <div className={`${style["item"]} ${style["item-new"]}`}>
+      <Button type="primary" className={style["new-btn"]} title="Create new">
+        <Link
+          className={`${style["item"]} ${style["item-new"]}`}
+          href="/article/new"
+        >
+          <PlusOutlined className={style["new-btn-icon"]} />
+        </Link>
+      </Button>
+    </div>
+  );
+}
+
+export default function ArticleOverviewPage() {
+  const [list, setList] = useState<false | t_article_view[]>(false);
+  const [user, setUser] = useState<false | t_token_user>(false);
+
+  useEffect(() => {
+    Promise.all([getArticleList(), getUser()]).then(([_l, _u]) => {
+      setList(_l);
+      setUser(_u);
+    });
+  }, []);
 
   return (
     <div className={style["article-overview-page-root"]}>
@@ -41,20 +65,7 @@ export default async function ArticleOverviewPage() {
         <span className={style["overview-title"]}>ArticleOverview</span>
       </div>
       <section className={style["grid-container"]}>
-        <div className={`${style["item"]} ${style["item-new"]}`}>
-          <Button
-            type="primary"
-            className={style["new-btn"]}
-            title="Create new"
-          >
-            <Link
-              className={`${style["item"]} ${style["item-new"]}`}
-              href="/article/new"
-            >
-              <PlusOutlined className={style["new-btn-icon"]} />
-            </Link>
-          </Button>
-        </div>
+        {user ? renderCreateBtn() : null}
         {renderActicleList(list)}
       </section>
     </div>
--- components/menu.tsx
+++ components/menu.tsx
@@ -9,22 +9,22 @@ export default function Menu() {
         <UseSVG name="house" />
       </Link>
       <div className={style.list}>
-        <Link href="/article">
-          <UseSVG name="github" />
-        </Link>
-        <Link href="/resume">
-          <UseSVG name="file-lines" />
-        </Link>
-        <Link href="/shop">
-          <UseSVG name="shopify" />
-        </Link>
-        <Link href="/chatroom">
-          <UseSVG name="comments" />
-        </Link>
-        <Link href="/login">
+        <Link href="/login" title="login">
           <UseSVG name="user" />
         </Link>
-        <Link href="https://github.com/4Nameless4">
+        <Link href="/article" title="article">
+          <UseSVG name="article" />
+        </Link>
+        <Link href="/resume" title="resume">
+          <UseSVG name="file-lines" />
+        </Link>
+        <Link href="/shop" title="shop">
+          <UseSVG name="shopify" />
+        </Link>
+        <Link href="/chatroom" title="chatroom">
+          <UseSVG name="comments" />
+        </Link>
+        <Link href="https://github.com/4Nameless4" title="github">
           <UseSVG name="github" />
         </Link>
       </div>
